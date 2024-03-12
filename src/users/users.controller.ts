85a7c3c9a2f50695147c2ed8791cff57743edceb
Grey Boolean <greyboolean@gmail.com>
2024-03-12 18:10:50 +0530

    set up a globally scoped interceptor to get current user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,7 @@ import {
   Post,
   Query,
   Session,
-  UseInterceptors,
+  // UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -18,12 +18,12 @@ import { Serialize } from '../interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
+// @UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
